28ec395d9b945f1f30387abcdb1ac67c7b5e0237
bqqbarbhg <samuli.1995@hotmail.com>
2017-06-04 22:57:08 +0300

    Skeleton.

--- src/smol-test.c
+++ src/smol-test.c
@@ -2,7 +2,7 @@
 #include "smol.h"
 #include <stdio.h>
 
-void square(smol_ctx *C, void*)
+void squareFn(smol_ctx *C, void*p)
 {
 	int x;
 	if (!smolArg(C, 0, "i", &x)) return;
@@ -14,11 +14,11 @@ int main(int argc, char **argv)
 	int result;
 	smol_ctx *C = smolInit();
 
-	smolPush(C, "n", &square, NULL);
+	smolPush(C, "n", &squareFn, NULL);
 	smolPop(C, "G", "square");
 
 	smolPush(C, "Gi", "square", 5);
-	smolCall(C, 2);
+	smolCall(C, 1);
 	smolPop(C, "i", &result);
 
 	smolDestroy(C);
--- src/smol.h
+++ src/smol.h
@@ -11,7 +11,9 @@ void smolDestroy(smol_ctx *C);
 
 void smolPush(smol_ctx *C, const char *fmt, ...);
 int smolPop(smol_ctx *C, const char *fmt, ...);
-int smolArg(smol_ctx *C, const char *fmt, ...);
+int smolDrop(smol_ctx *C, int n);
+int smolArg(smol_ctx *C, int index, const char *fmt, ...);
+int smolReturn(smol_ctx *C, const char *fmt, ...);
 
 void smolCall(smol_ctx *C, int numArgs);
 
@@ -20,9 +22,19 @@ void smolCall(smol_ctx *C, int numArgs);
 #if (defined(SMOL_IMPLEMENTATION) || defined(SMOL_INTERNAL)) && !defined(SMOL_IMPLEMENTATION_INCLUDED)
 #define SMOL_IMPLEMENTATION_INCLUDED
 
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 typedef struct smol_type smol_type;
 typedef struct smol_val smol_val;
 typedef struct smol_native smol_native;
+typedef struct smol_val_ptr smol_val_ptr;
+typedef struct smol_val_pair smol_val_pair;
 
 struct smol_native
 {
@@ -51,6 +63,7 @@ struct smol_ctx
 {
 	char *StackData;
 	smol_val_ptr *Stack;
+	char *DataP;
 	unsigned BP, SP;
 
 	char **GlobalNames;
@@ -62,11 +75,22 @@ struct smol_ctx
 
 smol_ctx *smolInit()
 {
+	smol_ctx *C;
 	C = (smol_ctx*)calloc(1, sizeof(smol_ctx));
+
+	C->StackData = (char*)calloc(16, 1024);
+	C->Stack = (smol_val_ptr*)calloc(1, sizeof(smol_val_ptr));
+	C->DataP = C->StackData;
+	C->BP = C->SP = 0;
+
+	C->GlobalNames = (char**)calloc(128, sizeof(char*));
+	C->GlobalValues = (smol_val_pair**)calloc(128, sizeof(smol_val_pair*));
+	C->GlobalCount = 0;
+
 	C->Int = (smol_type*)calloc(1, sizeof(smol_type));
-	C->Int.Size = sizeof(int);
+	C->Int->Size = sizeof(int);
 	C->Native = (smol_type*)calloc(1, sizeof(smol_type));
-	C->Native.Size = sizeof(smol_native);
+	C->Native->Size = sizeof(smol_native);
 }
 
 void smolDestroy(smol_ctx *C)
@@ -74,10 +98,11 @@ void smolDestroy(smol_ctx *C)
 	free(C);
 }
 
-void smol_StackWrite(smol_ctx *C, int start, const char *fmt, va_list va)
+int smol_StackWrite(smol_ctx *C, const char *fmt, va_list va)
 {
-	int pos = start;
-	char *datap = (char*)C->Stack[pos].Value;
+	int start = C->SP, pos = start;
+	char *datap = C->DataP;
+	smol_val_ptr *val;
 
 	for (const char *c = fmt; *c; c++) {
 		smol_type *type = NULL;
@@ -97,7 +122,7 @@ void smol_StackWrite(smol_ctx *C, int start, const char *fmt, va_list va)
 			type = C->Int;
 			} break;
 
-		case 'g': {
+		case 'G': {
 			int i;
 			const char *name = va_arg(va, const char*);
 			for (i = 0; i < C->GlobalCount; i++) {
@@ -113,29 +138,139 @@ void smol_StackWrite(smol_ctx *C, int start, const char *fmt, va_list va)
 		}
 
 		assert(type != NULL);
+		val->Type = type;
+		val->Value = datap;
 
+		pos++;
 		datap += type->Size;
 		datap += (8 - (uintptr_t)datap % 8) % 8;
 	}
+
+	C->DataP = datap;
+	C->SP = pos;
+	return pos - start;
 }
 
-void smol_StackRead(smol_ctx *C, int start, int dir, int end, const char *fmt, va_list va)
+int smol_StackRead(smol_ctx *C, int start, int dir, int end, const char *fmt, va_list va)
 {
+	int pos = start;
+	char *datap = (char*)C->Stack[pos].Value;
+	smol_val_ptr *val;
+
+	for (const char *c = fmt; *c; c++) {
+		smol_type *type = NULL;
+
+		if (pos == end)
+			return -1;
+
+		val = &C->Stack[pos];
+
+		switch (*c) {
+
+		case 'n': {
+			smol_native *n = (smol_native*)val->Value;
+			*va_arg(va, smol_native_fn*) = n->Func;
+			*va_arg(va, void**) = n->Arg;
+			} break;
+
+		case 'i': {
+			*va_arg(va, int*) = *(int*)val->Value;
+			} break;
+
+		case 'G': {
+			int i;
+			const char *name = va_arg(va, const char*);
+			int inserted = 0, num = C->GlobalCount;
+
+			for (i = 0; i < num; i++) {
+				if (!strcmp(C->GlobalNames[i], name)) {
+					smol_val_pair *p = C->GlobalValues[i];
+					memcpy(p->Value, val->Value, val->Type->Size);
+					inserted = 1;
+					break;
+				}
+			}
+
+			if (!inserted) {
+				smol_type *t = val->Type;
+				smol_val_pair *p = (smol_val_pair*)malloc(sizeof(smol_type*) + t->Size);
+				p->Type = val->Type;
+				memcpy(p->Value, val->Value, t->Size);
+				C->GlobalNames[num] = strdup(name);
+				C->GlobalValues[num] = p;
+				C->GlobalCount = num + 1;
+			}
+
+			}
+
+		}
+
+		pos += dir;
+	}
+
+	return dir > 0 ? pos - start : start - pos;
 }
 
 void smolPush(smol_ctx *C, const char *fmt, ...)
 {
+	int num;
+	va_list va;
+	va_start(va, fmt);
+	num = smol_StackWrite(C, fmt, va);
+	va_end(va);
 }
 
 int smolPop(smol_ctx *C, const char *fmt, ...)
 {
+	int num;
+	va_list va;
+	va_start(va, fmt);
+	num = smol_StackRead(C, C->SP - 1, -1, C->BP - 1, fmt, va);
+	va_end(va);
+
+	smolDrop(C, num);
+
+	return num >= 0;
 }
 
-int smolArg(smol_ctx *C, const char *fmt, ...)
+int smolDrop(smol_ctx *C, int n)
 {
+	C->SP -= n;
+	C->DataP = (char*)C->Stack[C->SP].Value;
 }
 
-void smolCall(smol_ctx *C, int numArgs);
+int smolArg(smol_ctx *C, int index, const char *fmt, ...)
+{
+	int num;
+	va_list va;
+	va_start(va, fmt);
+	num = smol_StackRead(C, C->BP, 1, C->SP, fmt, va);
+	va_end(va);
+
+	return num >= 0;
+}
+
+int smolReturn(smol_ctx *C, const char *fmt, ...)
+{
+	int num;
+	va_list va;
+
+	C->SP = C->BP;
+
+	va_start(va, fmt);
+	num = smol_StackWrite(C, fmt, va);
+	va_end(va);
+}
+
+void smolCall(smol_ctx *C, int numArgs)
+{
+	smol_val_ptr *val = &C->Stack[C->SP - numArgs - 1];
+	smol_native *n = (smol_native*)val->Value;
+	int bp = C->BP;
+	C->BP = C->SP - numArgs;
+	n->Func(C, n->Arg);
+	C->BP = bp;
+}
 
 #endif
 
